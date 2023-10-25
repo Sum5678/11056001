b9c5ed4bf95122d936c6e18e9f5aa97207c1c608
Sum5678 <11056001@ntub.edu.tw>
2023-10-25 16:16:12 +0800

    1025 我不知道

--- my0927/settings.py
+++ my0927/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT=BASE_DIR/"staticfile"
+STATICFILES_DIRS=[
+    BASE_DIR/"static"
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
